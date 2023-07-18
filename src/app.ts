77455ae7d921bd6b3c188610c8eb3de72fd1363a
Jefferson Santos <jeffersonduarte@id.uff.br>
2023-07-17 23:40:01 -0300

    pdf download curriculo

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import "express-async-errors";
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import router from "./routers/router";
 import morgan from "morgan";
 import helmet from "helmet";
+
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 
@@ -17,10 +20,36 @@ app.use(helmet());
 
 app.use("/api", router);
 
-app.get("/", (request: Request, response: Response) => {
-    response.status(200).json({
-        message: "Messages Api",
-    });
+app.get('/curriculoptbr', (req: Request, res: Response) => {
+  const filePath = path.join(__dirname, '../assets', 'Portugues.pdf');
+
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      res.status(404).send('Arquivo não encontrado.');
+    } else {
+      res.download(filePath, 'Português-version.pdf', (err) => {
+        if (err) {
+          res.status(500).send('Erro ao realizar o download.');
+        }
+      });
+    }
+  });
 });
+app.get('/curriculoenglish', (req: Request, res: Response) => {
+    const filePath = path.join(__dirname, '../assets', 'English.pdf');
+  
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        res.status(404).send('Arquivo não encontrado.');
+      } else {
+        res.download(filePath, 'English-version.pdf', (err) => {
+          if (err) {
+            res.status(500).send('Erro ao realizar o download.');
+          }
+        });
+      }
+    });
+  });
+
 
 export default app;
